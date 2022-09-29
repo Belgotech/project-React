b96bf22a4d2790038c80b333da4ab73366211602
Belgotech <Belovedobiora2002@gmail.com>
2022-09-29 09:09:52 -0700

    Fetching the api for the movie

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useEffect } from "react";
+// import "./App.css";
+// import Nav from "./Components/Nav/Nav";
+const API_URL = "https://www.omdbadi.com?apikey=";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const searchMovie = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+    console.log(data.search);
+  };
+
+  useEffect(() => {
+    searchMovie("Batman");
+  }, []);
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
-}
+      <h1>Movie Zone</h1>
 
-export default App
+      <div className="search">
+        <input
+          type="text"
+          placeholder="search for movies"
+          value="Superman"
+          onchange={() => {}}
+        />
+        <img src={searchIcon} alt="search" onClick={() => {}} />
+      </div>
+
+      <div className="container">
+        
+      </div>
+    </div>
+  );
+};
+
+export default App;
