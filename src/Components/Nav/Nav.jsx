194f9ddbf892035869549c598089aae245f53d3c
Belgotech <Belovedobiora2002@gmail.com>
2022-10-02 17:43:13 -0700

    setting up

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -9,3 +9,22 @@ const Nav = () => {
 }
 
 export default Nav
+
+
+
+
+// import React from 'react'
+
+// const Nav = (props) => {
+//     return (
+//         <div>
+//             <>
+//       <h1>name: {props.name}</h1>
+//       <h2>lastName: {props.lastName}</h2>
+//       <h3>age: {props.age}</h3>
+//       </>
+//         </div>
+//     )
+// }
+
+// export default Nav
